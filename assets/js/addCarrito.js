ea40823d9de06eafcf3e6b30d91c391fdaf7920a
Samuel Quezada <squezada.dw@gmail.com>
2026-03-13 15:06:01 -0300

    Adición de estilos y preparación de funciones JS

---
+++ assets/js/addCarrito.js
@@ -0,0 +1,26 @@
+const anadirNaruto = document.querySelector('#addNaruto');
+const anadirJujutsu = document.querySelector('#addJujutsu');
+const anadirTensura = document.querySelector('#addTensura');
+const anadirKimetsu = document.querySelector('#addKimetsu');
+
+//Añadir Naruto
+anadirNaruto.addEventListener('click', () => {
+
+});
+
+//Añadir Jujutsu
+anadirJujutsu.addEventListener('click', () => {
+
+});
+
+//Añadir Tensura
+anadirJujutsu.addEventListener('click', () => {
+
+});
+
+//Añadir Kimetsu
+anadirKimetsu.addEventListener('click', () => {
+    
+});
+
+
