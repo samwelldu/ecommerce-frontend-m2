b026cc45234b30442bf9de418967d809bfa29b30
Samuel Quezada <squezada.dw@gmail.com>
2026-03-14 15:17:38 -0300

    Eliminación de funciones que no dicta el ejercicio, hacer código más simple.

--- assets/js/addCarrito.js
+++ assets/js/addCarrito.js
@@ -1,87 +1,79 @@
+// Aquí declaro los botones de "añadir al carrito", de cada producto
 const anadirNaruto = document.querySelector('#addNaruto');
 const anadirJujutsu = document.querySelector('#addJujutsu');
 const anadirTensura = document.querySelector('#addTensura');
 const anadirKimetsu = document.querySelector('#addKimetsu');
+
+
+// Este es el botón para limpiar el carrito :3
 const botonLimpiar = document.querySelector('#limpiarCarrito');
 
-let contadorVisual = document.querySelector('#contadorCarrito');
+// Aquí declaro el contador visual del carrito, al badge del HTML
+const contadorVisual = document.querySelector('#contadorCarrito');
+const contadorVisualNaruto = document.querySelector('#cantidadCarritoNaruto');
+const contadorVisualJujutsu = document.querySelector('#cantidadCarritoJujutsu');
+const contadorVisualTensura = document.querySelector('#cantidadCarritoTensura');
+const contadorVisualKimetsu = document.querySelector('#cantidadCarritoKimetsu');
 
+// Aquí declaramos el contador general y el contador de cada producto
 let carritoCount = Number(localStorage.getItem('carrito-contador')) || 0;
-let carritoCountNaruto = Number(localStorage.getItem('naruto-contador')) || 0;
-let carritoCountJujutsu = Number(localStorage.getItem('jujutsu-contador')) || 0;
-let carritoCountTensura = Number(localStorage.getItem('tensura-contador')) || 0;
-let carritoCountKimetsu = Number(localStorage.getItem('kimetsu-contador')) || 0;
 
+// Aquí manifiesto que el span/texto del contador visual debe mostrar la variable del contador del carrito, para que se vaya actualizando
 contadorVisual.innerText = carritoCount;
 
-//Añadir Naruto
+
+// Botón de añadir al carrito Naruto
 anadirNaruto.addEventListener('click', () => {
     carritoCount++;
-    carritoCountNaruto++;
 
     localStorage.setItem('carrito-contador', carritoCount);
-    localStorage.setItem('naruto-contador', carritoCountNaruto);
 
-    console.log('Naruto: ' + carritoCountNaruto);
     console.log('Carrito total: ' + carritoCount);
     contadorVisual.innerText = carritoCount;
 });
 
-//Añadir Jujutsu
+// Botón de añadir al carrito Jujutsu Kaisen
 anadirJujutsu.addEventListener('click', () => {
     carritoCount++;
-    carritoCountJujutsu++;
 
     localStorage.setItem('carrito-contador', carritoCount);
-    localStorage.setItem('jujutsu-contador', carritoCountJujutsu);
 
-    console.log('Naruto: ' + carritoCountJujutsu);
     console.log('Carrito total: ' + carritoCount);
     contadorVisual.innerText = carritoCount;
 });
 
-//Añadir Tensura
+// Botón de añadir al carrito Tensura
 anadirTensura.addEventListener('click', () => {
     carritoCount++;
-    carritoCountTensura++;
 
     localStorage.setItem('carrito-contador', carritoCount);
-    localStorage.setItem('tensura-contador', carritoCountTensura);
 
-    console.log('Naruto: ' + carritoCountTensura);
     console.log('Carrito total: ' + carritoCount);
     contadorVisual.innerText = carritoCount;
 });
 
-//Añadir Kimetsu
+// Botón de añadir al carrito Kimetsu no Yaiba
 anadirKimetsu.addEventListener('click', () => {
     carritoCount++;
-    carritoCountKimetsu++;
 
     localStorage.setItem('carrito-contador', carritoCount);
-    localStorage.setItem('kimetsu-contador', carritoCountKimetsu);
 
-    console.log('Naruto: ' + carritoCountKimetsu);
     console.log('Carrito total: ' + carritoCount);
     contadorVisual.innerText = carritoCount;
 });
 
-//Botón para limpiar el carrito
 
+// Función flecha para el click de Limpiar el localStorage (carrito)
 botonLimpiar.addEventListener('click', () => {
     localStorage.clear();
 
-    // Estaba usando esta, pero siento que es muy tosca: location.reload();
+    //location.reload(); Estaba usando esta, pero no se ve pro
 
+    //Reinicio los datos, para que no debamos actualizar la página.
     carritoCount = 0;
-    carritoCountNaruto = 0;
-    carritoCountJujutsu = 0;
-    carritoCountTensura = 0;
-    carritoCountKimetsu = 0;
 
     contadorVisual.innerText = carritoCount;
 
     console.log('Has limpiado el localStorage de este sitio correctamente');
-    
-
 });
+
